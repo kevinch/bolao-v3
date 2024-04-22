00091dc84cbde9e8d5201fbadebbd8782156e145
kevin <kevinchevallier@gmail.com>
2024-04-22 18:07:23 -0300

    Update results page to SSR

--- app/bolao/[id]/results/page.tsx
+++ app/bolao/[id]/results/page.tsx
@@ -1,36 +1,14 @@
-"use client"
-
 import Nav from "@/app/components/nav"
 import { fetchBolao } from "@/app/lib/data"
-import { useEffect, useState } from "react"
 
-interface Bolao {
-  name: string
-  id: string
-  competition_id: string
+async function getData(bolaoId: string) {
+  const res = await fetchBolao(bolaoId)
+
+  return res
 }
 
-function Results({ params }: { params: { id: string } }) {
-  const [data, setData] = useState<Bolao>({
-    name: "",
-    id: "",
-    competition_id: "",
-  })
-  const bolaoId = params.id
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response: Bolao = await fetchBolao(bolaoId)
-
-        setData(response)
-      } catch (error) {
-        console.error("Error fetching data:", error)
-      }
-    }
-
-    fetchData()
-  }, [])
+export async function Results({ params }: { params: { id: string } }) {
+  const data = await getData(params.id)
 
   if (!data) {
     return <p>Error while loading the bolao.</p>
@@ -40,7 +18,7 @@ function Results({ params }: { params: { id: string } }) {
     <main>
       <Nav />
       <h1>{data.name}</h1>
-      <pre>Competition id: {data.competition_id}</pre>
+      <pre>Competition Id: {data.competition_id}</pre>
     </main>
   )
 }
