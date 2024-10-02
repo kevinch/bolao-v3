a2b8c65529444931b06eddfdb72aacacc5a10993
kevin <kevinchevallier@gmail.com>
2024-10-02 08:21:37 -0300

    Move to tooltip & popover with button

--- app/ui/bolao/teamCodeLogo.tsx
+++ app/ui/bolao/teamCodeLogo.tsx
@@ -8,12 +8,12 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
-// import {
-//   Popover,
-//   PopoverContent,
-//   PopoverTrigger,
-// } from "@/components/ui/popover"
-// import { isBrowser } from "react-device-detect"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { isBrowser } from "react-device-detect"
 
 type Props = {
   name: string
@@ -36,70 +36,70 @@ function TeamCodeLogo({ name, logoSrc }: Props) {
     />
   )
 
-  return (
-    <>
-      <TooltipProvider delayDuration={0}>
-        <Tooltip open={open}>
-          <TooltipTrigger asChild>
-            <span className="flex justify-center flex-col mx-3">
-              <span className="flex justify-center">
-                <button
-                  type="button"
-                  className="cursor-pointer"
-                  onClick={() => setOpen(!open)}
-                  onMouseEnter={() => setOpen(true)}
-                  onMouseLeave={() => setOpen(false)}
-                  onTouchStart={() => setOpen(!open)}
-                  onKeyDown={(e) => {
-                    e.preventDefault()
-                    e.key === "Enter" && setOpen(!open)
-                  }}
-                >
-                  {triggerElement}
-                </button>{" "}
-              </span>
-              <span className="text-sm text-center">
-                {formatTeamCode(name)}
-              </span>
-            </span>
-          </TooltipTrigger>
-          <TooltipContent>
-            <span className="inline-block">{name}</span>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-    </>
-  )
-
-  // if (isBrowser) {
-  //   return (
-  //     <span className="flex justify-center flex-col mx-3">
-  //       <span className="flex justify-center">
-  //         <TooltipProvider delayDuration={0}>
-  //           <Tooltip>
-  //             <TooltipTrigger asChild>{triggerElement}</TooltipTrigger>
-  //             <TooltipContent>{name}</TooltipContent>
-  //           </Tooltip>
-  //         </TooltipProvider>
-  //       </span>
-  //       <span className="text-sm text-center">{formatTeamCode(name)}</span>
-  //     </span>
-  //   )
-  // }
-
   // return (
-  //   <Popover>
-  //     <PopoverTrigger asChild>
-  //       <span className="flex justify-center flex-col mx-3">
-  //         <span className="flex justify-center">{triggerElement}</span>
-  //         <span className="text-sm text-center">{formatTeamCode(name)}</span>
-  //       </span>
-  //     </PopoverTrigger>
-  //     <PopoverContent className="w-auto">
-  //       <span>{name}</span>
-  //     </PopoverContent>
-  //   </Popover>
+  //   <>
+  //     <TooltipProvider delayDuration={0}>
+  //       <Tooltip open={open}>
+  //         <TooltipTrigger asChild>
+  //           <span className="flex justify-center flex-col mx-3">
+  //             <span className="flex justify-center">
+  //               <button
+  //                 type="button"
+  //                 className="cursor-pointer"
+  //                 onClick={() => setOpen(!open)}
+  //                 onMouseEnter={() => setOpen(true)}
+  //                 onMouseLeave={() => setOpen(false)}
+  //                 onTouchStart={() => setOpen(!open)}
+  //                 onKeyDown={(e) => {
+  //                   e.preventDefault()
+  //                   e.key === "Enter" && setOpen(!open)
+  //                 }}
+  //               >
+  //                 {triggerElement}
+  //               </button>{" "}
+  //             </span>
+  //             <span className="text-sm text-center">
+  //               {formatTeamCode(name)}
+  //             </span>
+  //           </span>
+  //         </TooltipTrigger>
+  //         <TooltipContent>
+  //           <span className="inline-block">{name}</span>
+  //         </TooltipContent>
+  //       </Tooltip>
+  //     </TooltipProvider>
+  //   </>
   // )
+
+  if (isBrowser) {
+    return (
+      <span className="flex justify-center flex-col mx-3">
+        <span className="flex justify-center">
+          <TooltipProvider delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger asChild>{triggerElement}</TooltipTrigger>
+              <TooltipContent>{name}</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </span>
+        <span className="text-sm text-center">{formatTeamCode(name)}</span>
+      </span>
+    )
+  }
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <button className="flex justify-center flex-col mx-3">
+          <span className="flex justify-center">{triggerElement}</span>
+          <span className="text-sm text-center">{formatTeamCode(name)}</span>
+        </button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto">
+        <span>{name}</span>
+      </PopoverContent>
+    </Popover>
+  )
 }
 
 export default TeamCodeLogo
