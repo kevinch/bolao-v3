81b26eca503e9f66c116f9c07602e102e3ae0f4e
kevin <kevinchevallier@gmail.com>
2024-10-02 07:49:56 -0300

    Add popover for mobile devices

--- app/ui/bolao/teamCodeLogo.tsx
+++ app/ui/bolao/teamCodeLogo.tsx
@@ -5,6 +5,12 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { BrowserView, MobileView } from "react-device-detect"
 
 type Props = {
   name: string
@@ -18,21 +24,38 @@ function TeamCodeLogo({ name, logoSrc }: Props) {
   return (
     <span className="flex justify-center flex-col mx-3">
       <span className="flex justify-center">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
+        <BrowserView>
+          <TooltipProvider delayDuration={0}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Image
+                  width={20}
+                  height={20}
+                  src={logoSrc}
+                  alt={`${name}'s logo`}
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{name}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </BrowserView>
+        <MobileView>
+          <Popover>
+            <PopoverTrigger asChild>
               <Image
                 width={20}
                 height={20}
                 src={logoSrc}
                 alt={`${name}'s logo`}
               />
-            </TooltipTrigger>
-            <TooltipContent>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
               <p>{name}</p>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+            </PopoverContent>
+          </Popover>
+        </MobileView>
       </span>
       <span className="text-sm">{formatTeamCode(name)}</span>
     </span>
