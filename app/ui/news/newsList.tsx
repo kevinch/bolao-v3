8cd0de5f251f8df6bfabb97deb773cb1b71aee68
kevin <kevinchevallier@gmail.com>
2024-10-03 07:42:12 -0300

    Fix typings of news

--- app/ui/news/newsList.tsx
+++ app/ui/news/newsList.tsx
@@ -2,52 +2,14 @@
 
 import { formatDateNews } from "@/app/lib/utils"
 import { PrismicRichText } from "@prismicio/react"
+import type { NewsDocument } from "@/prismicio-types"
 
-function NewsList({
-  documents,
-}: {
-  documents: any
-  // [
-  // {
-  // id: string
-  // uid: string
-  // url: string
-  // type: string
-  // href: string
-  // tags: []
-  // first_publication_date: string
-  // last_publication_date: string
-  // slugs: []
-  // linked_documents: []
-  // lang: string
-  // alternate_languages: []
-  // data: any
-  // {
-  //   title: [
-  //     {
-  //       type: string
-  //       text: string
-  //       spans: []
-  //       direction: string
-  //     },
-  //   ],
-  //   content: [
-  //     {
-  //       type: string
-  //       text: string
-  //       spans: []
-  //       direction: string
-  //     }
-  //   ]
-  // },
-  //   },
-  // ]
-}) {
+function NewsList({ documents }: { documents: NewsDocument[] }) {
   return (
-    <div>
+    <>
       {documents.map((document: any) => {
         return (
-          <div className="mb-6" key={document.id}>
+          <div className="mb-20" key={document.id}>
             <h2 className="text-2xl">{document.data.title[0]?.text}</h2>
             <div className="text-sm mb-12 text-slate-500">
               {formatDateNews(document.first_publication_date)}
@@ -57,7 +19,7 @@ function NewsList({
           </div>
         )
       })}
-    </div>
+    </>
   )
 }
 
