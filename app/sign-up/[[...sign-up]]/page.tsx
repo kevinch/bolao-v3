e4bc2313f7957516c38fd6f485718f06ca5f205d
kevin <kevinchevallier@gmail.com>
2024-10-12 13:33:46 -0300

    Revert signup page after disabling verify at sign up verification on clerk dashboard

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,31 +1,5 @@
-"use client"
-
-import { SignUp, useSignUp } from "@clerk/nextjs"
+import { SignUp } from "@clerk/nextjs"
 import PageTitle from "@/app/components/pageTitle"
-import { useSearchParams, useRouter } from "next/navigation"
-import { useEffect } from "react"
-
-function SignUpWrapper() {
-  const { isLoaded, signUp, setActive } = useSignUp()
-  const searchParams = useSearchParams()
-  const router = useRouter()
-  const redirectUrl = searchParams.get("redirect_url")
-
-  useEffect(() => {
-    if (isLoaded && signUp?.status === "complete") {
-      setActive({ session: signUp.createdSessionId })
-        .then(() => {
-          router.push(redirectUrl || "/")
-        })
-        .catch((error) => {
-          console.error("Error setting active session", error)
-          router.push("/")
-        })
-    }
-  }, [isLoaded, signUp, setActive, router, redirectUrl])
-
-  return <SignUp routing="path" path="/sign-up" />
-}
 
 export default function Page() {
   return (
@@ -33,7 +7,7 @@ export default function Page() {
       <PageTitle>Register</PageTitle>
 
       <div className="flex justify-center">
-        <SignUpWrapper />
+        <SignUp />
       </div>
     </main>
   )
